252bd6145b20101e07c54e898b9c0ff9ed8a8598
rehna-jp <jeremyrehna05@gmail.com>
2024-12-03 20:00:54 +0000

    added a javascript file

---
+++ Script.js
@@ -0,0 +1,10 @@
+function showMe(){
+    // var f = documents.forms ["form"]["email"].value;
+
+    alert("Thank you for subscribing to our newsletter. We will be sending regular child care news and health care advice from carefully chosen Industry experts to your email");
+}
+
+function message(){
+     
+    alert("Thank you for sending us a message. We'll get right back to you");
+}
